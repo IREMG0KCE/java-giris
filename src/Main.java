cd8e95c31a2328086c298ecf1b465493c6f2ddcd
İrem Gökçe <iremiskenderun@gmail.com>
2025-12-30 13:27:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+ import java.util.Arrays;
+import java.util.Scanner;// kullanıcıdan veri girişi almak için
+
+public class Main{
+    public static void main(String[] args){
+     Scanner input = new Scanner(System.in);
+     int sayi1,sayi2;
+     System.out.println("ilk sayi giriniz:");
+     sayi1 = input.nextInt();
+     System.out.println("ikinci sayi giriniz:");
+     sayi2 = input.nextInt();
+        int[] sayilar = {sayi1, sayi2};
+        double ort = Arrays.stream(sayilar).average().orElse(0);
+     System.out.println("ortalama sonuç:"+ (int) ort);
+    }
+}
